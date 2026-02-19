e1e85f5c6cce368ece8ac94763bcdbd4587c9c49
DanieduUSB <20-10518@usb.ve>
2026-02-19 04:40:45 +0000

    Laboratorio #1 20-10518

---
+++ lab1/DegreesOfSeparation.kt
@@ -0,0 +1,69 @@
+import java.io.File
+import java.io.BufferedReader
+
+fun loadGraph(): ListaAdyacenciaGrafo<String> {
+    val file = File("input.txt")
+    val reader = file.bufferedReader()
+    val graph = ListaAdyacenciaGrafo<String>()
+    var line = reader.readLine()
+    while (line != null) {
+        val names: List<String> = line.split(" ")
+        if (names.size != 2) error("Formato de archivo inválido")
+        graph.agregarVertice(names[0])
+        graph.agregarVertice(names[1])
+        graph.conectar(names[0],names[1])
+        graph.conectar(names[1],names[0])
+        line = reader.readLine()
+    }
+    return graph
+}
+
+fun degreesOfSeparation(graph: Grafo<String>, nameA: String, nameB: String): Int {
+    if (!graph.contiene(nameA) || !graph.contiene(nameB)) return -1
+    if (nameA == nameB) return 0
+
+    val visited: MutableMap<String, Boolean> = mutableMapOf()
+    visited[nameA] = true
+
+    val queue = ArrayDeque<String>()
+    queue.addLast(nameA)
+
+    var toCheckCurrentLevel = 1
+    var toCheckNextLevel = 0
+    var degreesOfSeparation = 1
+
+    while(queue.isNotEmpty()) {
+        val vertex = queue.removeFirst()
+
+        for (sucesor in graph.obtenerArcosSalida(vertex)) {
+            if (sucesor == nameB) return degreesOfSeparation
+
+            if (visited[sucesor] == null) {
+                visited[sucesor] = true
+                toCheckNextLevel++
+                queue.addLast(sucesor)
+            }
+        }
+
+        toCheckCurrentLevel--
+        if (toCheckCurrentLevel == 0) {
+            degreesOfSeparation++
+            toCheckCurrentLevel = toCheckNextLevel
+            toCheckNextLevel = 0
+        }
+    }
+    return -1
+}
+
+fun main(args: Array<String>) {
+    if (args.size != 2) {
+        println("Debe introducir dos nombres")
+        return
+    }
+
+    val graph = loadGraph()
+    val nameA = args[0]
+    val nameB = args[1]
+
+    println(degreesOfSeparation(graph, nameA, nameB))
+}
---
+++ lab1/README.md
@@ -0,0 +1,22 @@
+Laboratorio #1 (CI2693)
+Daniel Quijada (20-10518)
+
+Instrucciones para COMPILAR el programa (Desde terminal en Linux)
+
+	1- Abrir la terminal de la distribución de Linux correspondiente
+
+	2- Ingresar en la carpeta en la que se encuentre el laboratorio con el comando cd
+
+	3- Escribir el comando 'make' en la terminal
+
+Instrucciones para CORRER el programa (Desde terminal en Linux)
+
+	1- Compilar el programa
+
+	2- Colocar en la carpeta del laboratorio el archivo 'input.txt' (Ver Instrucciones input.txt para mas información al respecto)
+
+	3- Usar el comando './run.sh (nombreA) (nombreB)'. Si no se especifican exactamente dos nombres, el programa terminará la ejecución con un error.
+
+Instrucciones input.txt
+
+	1- 
